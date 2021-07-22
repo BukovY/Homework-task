81f1ac2ca1b1d8abf83dcdd833b201d6c8153b64
Быков Евгений <76494975+BukovY@users.noreply.github.com>
2021-07-23 00:35:55 +0300

    + format

--- film-epam/src/App.js
+++ film-epam/src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
-import './App.css';
-import {store} from "./redux/store";
+import "./App.css";
+import { store } from "./redux/store";
 import Homepage from "./Pages/Homepage/Homepage";
 import Header from "./components/Header/Header";
 import Movie from "./Pages/Movie/Movie";
 
 function App() {
-    const page = store.selectPage
-    return (
-        <div>
-            <Header/>
-            {page === 'main' ? <Homepage/> : ''}
-            {page === 'actor' ? '' : ''}
-            {page === 'movie' ? <Movie/> : ''}
-        </div>
-    );
+  const page = store.selectPage;
+  return (
+    <div>
+      <Header />
+      {page === "main" ? <Homepage /> : ""}
+      {page === "actor" ? "" : ""}
+      {page === "movie" ? <Movie /> : ""}
+    </div>
+  );
 }
 
 export default App;
