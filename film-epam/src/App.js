88aed3d2e7137eb25e352d9cbe4bdeac95621541
Быков Евгений <76494975+BukovY@users.noreply.github.com>
2021-07-22 23:44:40 +0300

    change H1 -> h1

--- film-epam/src/App.js
+++ film-epam/src/App.js
@@ -1,9 +1,10 @@
+import React from "react";
 import "./App.css";
 
 const App = () => {
   return (
     <>
-      <H1>Some h121213</H1>
+      <h1>Some h121213</h1>
       <div>Format Window</div>
     </>
   );
