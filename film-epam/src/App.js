384a056e1d9b1f911c933796fc8bad5f230d6961
Быков Евгений <76494975+BukovY@users.noreply.github.com>
2021-07-22 23:28:56 +0300

    add formating

--- film-epam/src/App.js
+++ film-epam/src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <h1>Someh121213</h1>
+    <>
+      <h1>Someh121213</h1>
+      <div>sdfdfd</div>
+      sgssfg hjfjhfdhjd
+    </>
   );
 }
 
