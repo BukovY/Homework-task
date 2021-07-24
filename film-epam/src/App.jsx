4604786993b3e3119d73e02905ff1b9d2bebcac2
Быков Евгений <76494975+BukovY@users.noreply.github.com>
2021-07-24 23:44:44 +0300

    add Error bound. but i am not shure(

--- film-epam/src/App.jsx
+++ film-epam/src/App.jsx
@@ -5,15 +5,16 @@ import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage/HomePage.jsx";
 import MoviePage from "./pages/MoviePage/MoviePage.jsx";
 import ActorPage from "./pages/ActorPage/ActorPage";
+import {ErrorBoundary} from "./components/ErrorBoundary/Error Boundary";
 
 const App = () => {
   const page = store.selectPage;
   return (
     <div>
       <Header />
-      {page === "main" ? <HomePage /> : ""}
-      {page === "actor" ? <ActorPage/> : ""}
-      {page === "movie" ? <MoviePage /> : ""}
+      {page === "main" ? <ErrorBoundary><HomePage /></ErrorBoundary> : ""}
+      {page === "actor" ? <ErrorBoundary><ActorPage/></ErrorBoundary> : ""}
+      {page === "movie" ? <ErrorBoundary><MoviePage /></ErrorBoundary> : ""}
     </div>
   );
 };
