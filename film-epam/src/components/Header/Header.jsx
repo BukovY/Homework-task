542981575e21523ab075768f19b73e853c810202
Быков Евгений <76494975+BukovY@users.noreply.github.com>
2021-08-13 23:36:08 +0300

    start use material u i

--- film-epam/src/components/Header/Header.jsx
+++ film-epam/src/components/Header/Header.jsx
@@ -1,24 +1,48 @@
 import React from "react";
-import logo from "../../static/img/logo.png";
 import styles from "./Header.module.css.sass";
 import { Language } from "../Language";
 import { Search } from "../Search";
 import { useDispatch } from "react-redux";
 import { resetFilters } from "../../redux/actions/appAction";
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  title: {
+    cursor: "pointer",
+    "& a": {
+      color: "white",
+      textDecoration: "none",
+    },
+  },
+}));
+
 export const Header = () => {
+  const classes = useStyles();
   const dispatch = useDispatch();
   const goHomepage = () => {
     dispatch(resetFilters());
   };
   return (
-    <header className={styles}>
-      <Link to="/">
-        <img src={logo} alt="logo" onClick={goHomepage} />
-      </Link>
-      <Search />
-      <Language />
-    </header>
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.flex}>
+        <Typography className={classes.title}>
+          <Link to={"/"} onClick={goHomepage}>
+            Film App
+          </Link>
+        </Typography>
+        <Search />
+        <Language />
+      </AppBar>
+    </div>
   );
 };
--- film-epam/src/components/Pagination/Paginations.jsx
+++ film-epam/src/components/Pagination/Paginations.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import styles from "./Paginations.module.sass";
-import { Pagination } from "./components/Parination";
-import { getPaginationRange } from "../../utils/functrions";
+import Pagination from "@material-ui/lab/Pagination";
 
 export const Paginations = ({ selected, max, handler }) => {
+  const handleChange = (e, value) => {
+    handler(value);
+  };
   return (
     <div className={styles.pagination_box}>
-      {getPaginationRange(max).map((el) => (
-        <Pagination
-          key={el}
-          num={el}
-          isSelect={el === selected}
-          handler={handler}
-        />
-      ))}
+      <Pagination
+        variant="outlined"
+        boundaryCount={max}
+        color="primary"
+        count={max}
+        defaultPage={selected}
+        onChange={handleChange}
+      />
     </div>
   );
 };
